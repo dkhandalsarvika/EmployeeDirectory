d5b35221f8f1c42e8a5e476a7ba120a7b9a7e4f1
Deepak Kumar Sharma <deepak.khandal3@gmail.com>
2019-12-29 03:05:38 +0530

    Alert box implemented on action bar of employee detail page with boolean flag of isAlertBoxRequired.

--- app/ActionBar.js
+++ app/ActionBar.js
@@ -1,26 +1,97 @@
 import React, {Component} from 'react';
 import {View, Text, Image, StyleSheet, TouchableOpacity, Linking,Alert} from 'react-native';
 
+var isAlertBoxRequired = true;
+
 export default class ActionBar extends Component {
 
+    
+
     callNumber() {
-        Alert.alert(this.props.phone);
-        //this.openURL('tel:' + this.props.phone);
+
+        if(isAlertBoxRequired == true){
+            //function to make two option alert
+            Alert.alert(
+              //title
+              'Call on personal number',
+              //body
+              'Do you want to call ?',
+              [
+                {text: 'Yes', onPress: () => this.openURL('tel:' + this.props.phone)},
+                {text: 'No', onPress: () => console.log('No Pressed personal number'), style: 'cancel'},
+              ],
+              { cancelable: false }
+              //clicking out side of alert will not cancel
+            );
+        }else{
+            this.openURL('tel:' + this.props.phone);
+        }
+
+        
     }
 
     callENumber() {
-        Alert.alert(this.props.ePhone);
-        //this.openURL('tel:' + this.props.ePhone);
+        if(isAlertBoxRequired == true){
+            //function to make two option alert
+            Alert.alert(
+              //title
+              'Call on emergency number',
+              //body
+              'Do you want to call ?',
+              [
+                {text: 'Yes', onPress: () => this.openURL('tel:' + this.props.ePhone)},
+                {text: 'No', onPress: () => console.log('No Pressed emergency number'), style: 'cancel'},
+              ],
+              { cancelable: false }
+              //clicking out side of alert will not cancel
+            );
+        }else{
+            this.openURL('tel:' + this.props.ePhone);
+        }
+
+        
     }
 
     sendMessage() {
-        Alert.alert(this.props.phone);
-        //this.openURL('sms:' + this.props.phone);
+        if(isAlertBoxRequired == true){
+            //function to make two option alert
+            Alert.alert(
+              //title
+              'Send message',
+              //body
+              'Do you want to send message ?',
+              [
+                {text: 'Yes', onPress: () => this.openURL('sms:' + this.props.phone)},
+                {text: 'No', onPress: () => console.log('No Pressed send message'), style: 'cancel'},
+              ],
+              { cancelable: false }
+              //clicking out side of alert will not cancel
+            );
+        }else{
+            this.openURL('sms:' + this.props.phone);
+        }
+
     }
 
     sendMail() {    
-        Alert.alert(this.props.email);
-        //this.openURL('mailto:' + this.props.email);
+        if(isAlertBoxRequired == true){
+            //function to make two option alert
+            Alert.alert(
+              //title
+              'Send email',
+              //body
+              'Do you want to send email ?',
+              [
+                {text: 'Yes', onPress: () => this.openURL('mailto:' + this.props.email)},
+                {text: 'No', onPress: () => console.log('No Pressed email'), style: 'cancel'},
+              ],
+              { cancelable: false }
+              //clicking out side of alert will not cancel
+            );
+        }else{
+            this.openURL('mailto:' + this.props.email);
+        }
+
     }
 
     openURL(url) {
